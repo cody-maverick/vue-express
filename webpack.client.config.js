4ab2b6b6eb5007bf33e17540347e9ef5fd17d053
a.matveev <a.matveev@12storeez.com>
2020-02-14 13:51:37 +0300

    555

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -5,15 +5,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const IS_DEV = process.env.NODE_ENV === 'development';
 const autoprefixer = require('autoprefixer');
-// const webpack = require('webpack');
 
 module.exports = merge(baseConfig, {
     entry: './src/entry-client.js',
     output: {
         sourceMapFilename: '[name].js.map',
-
-        // path: path.resolve(__dirname, '../dist'),
-        // publicPath: '/public/',
         path: path.resolve(__dirname, './dist'),
         publicPath: '/dist/',
         filename: 'main.js'
@@ -47,14 +43,21 @@ module.exports = merge(baseConfig, {
         ]
     },
     plugins:
-        [
-            new MiniCssExtractPlugin({
-                filename: '[name].css',
-            }),
-            new HtmlWebpackPlugin({
-                template: "./public/index.html"
-            })
-        ],
+        IS_DEV ?
+            [
+                new MiniCssExtractPlugin({
+                    filename: '[name].css',
+                }),
+                new HtmlWebpackPlugin({
+                    template: "./public/index.html"
+                })
+
+            ] : [
+                new MiniCssExtractPlugin({
+                    filename: '[name].css',
+                }),
+            ],
+
     devServer: {
         contentBase: path.join(__dirname, '/dist'),
         compress: true,
